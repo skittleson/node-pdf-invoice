a5c705941480d88cf76040b7254bc98b5860707e
Spencer Kittleson <spencerkittleson@hotmail.com>
2018-06-09 10:00:21 -0700

    better looking invoice

--- dist/i18n.js
+++ dist/i18n.js
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  en_US: {},
-  pt_BR: {
-    createdAt: 'Criado em',
-    amount: 'Total',
-    chargeStatus: 'Status de cobrança',
-    chargeFor: 'Cobrança para: ',
-    description: 'Descrição',
-    name: 'Nome',
-    quantity: 'Quantidade'
+  en_US: {
+    createdAt: "Created",
+    amount: "Total",
+    chargeStatus: "Test",
+    chargeFor: "asdf: ",
+    description: "asfd",
+    name: "Name",
+    quantity: "Quantity"
   }
 };
